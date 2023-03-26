5b2943d1f5ead3130527a14e706fd09c4be442cd
Tobias Merkle <tobias@branchmessenger.com>
2023-03-26 16:59:11 -0400

    cancel playing sound when press new btn

--- public/main.js
+++ public/main.js
@@ -1,35 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const soundboard = document.getElementById('soundboard');
-  
-    fetch('/sounds')
-      .then(response => response.json())
-      .then(sounds => {
-        const audioElements = {};
+  const soundboard = document.getElementById('soundboard');
+  let currentAudio;
 
-        sounds.forEach(sound => {
-          const audio = new Audio(sound.url);
-          audio.onerror = () => {
-            console.error(`Error loading audio file: ${sound.url}`);
-          };
-      
-          audioElements[sound.name] = audio;
-  
-          const buttonContainer = document.createElement('div');
-          buttonContainer.classList.add('button-container');
-  
-          const button = document.createElement('button');
-          button.classList.add('sound-button');
-          button.textContent = sound.name;
-          button.addEventListener('click', () => audio.play());
-          buttonContainer.appendChild(button);
-  
-          soundboard.appendChild(buttonContainer);
+  fetch('/sounds')
+    .then(response => response.json())
+    .then(sounds => {
+      const audioElements = {};
+
+      sounds.forEach(sound => {
+        const audio = new Audio(sound.url);
+        audio.onerror = () => {
+          console.error(`Error loading audio file: ${sound.url}`);
+        };
+        audioElements[sound.name] = audio;
+
+        const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('button-container');
+
+        const button = document.createElement('button');
+        button.classList.add('sound-button');
+        button.textContent = sound.name;
+        button.addEventListener('click', () => {
+          if (currentAudio && !currentAudio.paused) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+          }
+          audio.play();
+          currentAudio = audio;
         });
-  
-        Sortable.create(soundboard, {
-          animation: 150,
-          handle: '.sound-button',
-        });
+        buttonContainer.appendChild(button);
+        soundboard.appendChild(buttonContainer);
       });
-  });
-  
+
+      Sortable.create(soundboard, {
+        animation: 150,
+        handle: '.sound-button',
+      });
+    });
+});
