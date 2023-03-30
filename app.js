e55ee9584e541906f24e73331215eb35ad4c7bc3
Tobias Merkle <tobias@branchmessenger.com>
2023-03-30 01:19:31 -0400

    folder organization now possible

--- app.js
+++ app.js
@@ -3,27 +3,35 @@ const multer = require('multer');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
+const recursive = require('recursive-readdir');
+const isDirectory = require('is-directory');
 
 const app = express();
 const upload = multer();
 const soundboardDir = path.join(__dirname, 'public', 'soundboard');
 
-
 app.use(cors());
 app.use(express.static('public'));
 
 app.get('/sounds', (req, res) => {
-  fs.readdir(soundboardDir, (err, files) => {
+  const directoryPath = req.query.path ? path.join(soundboardDir, req.query.path) : soundboardDir;
+
+  recursive(directoryPath, (err, files) => {
     if (err) {
       return res.status(500).json({ error: 'Unable to read soundboard directory' });
     }
 
-    const sounds = files.map(file => ({
-      name: path.basename(file, path.extname(file)),
-      url: `/soundboard/${file}`,
-    }));
+    const items = files.map(file => {
+      const relativePath = file.replace(soundboardDir, '');
+      return {
+        type: isDirectory.sync(file) ? 'folder' : 'sound',
+        name: relativePath,
+        url: `/soundboard${relativePath}`,
+        relativePath: relativePath,
+      };
+    });
 
-    res.json(sounds);
+    res.json(items);
   });
 });
 
--- public/main.js
+++ public/main.js
@@ -1,48 +1,91 @@
+// main.js
 document.addEventListener('DOMContentLoaded', () => {
   const soundboard = document.getElementById('soundboard');
   const loadingContainer = document.getElementById('loading-container');
   const loadingBar = document.getElementById('loading-bar');
+  const breadcrumb = document.getElementById('breadcrumb');
   let currentAudio;
   let loadedCount = 0;
+  let currentPath = '';
+  
+  function getOrCreateHeading(text) {
+    let heading = document.querySelector(`h2[data-heading="${text}"]`);
+    if (!heading) {
+      heading = document.createElement('h2');
+      heading.setAttribute('data-heading', text);
+      heading.textContent = text;
+      soundboard.appendChild(heading);
+    }
+    return heading;
+  }
 
-  fetch('/sounds')
-    .then(response => response.json())
-    .then(sounds => {
-      const audioElements = {};
+  function fetchSounds(path) {
+    fetch(`/sounds${path ? '?path=' + encodeURIComponent(path) : ''}`)
+      .then(response => response.json())
+      .then(sounds => {
+        loadedCount = 0;
+        soundboard.innerHTML = '';
 
-      sounds.forEach(sound => {
-        const audio = new Audio(sound.url);
-        audio.onerror = () => {
-          console.error(`Error loading audio file: ${sound.url}`);
-        };
-        audio.onloadeddata = () => {
-          loadedCount++;
-          const progress = (loadedCount / sounds.length) * 100;
-          loadingBar.style.width = `${progress}%`;
-          if (loadedCount === sounds.length) {
-            setTimeout(() => {
-              loadingContainer.style.display = 'none';
-            }, 500);
+        sounds.forEach((sound) => {
+          let audio;
+          sound.name = sound.name.replace(/^\//, '')
+
+          if (sound.type === 'sound') {
+            audio = new Audio(sound.url);
+            audio.onerror = () => {
+              console.error(`Error loading audio file: ${sound.url}`);
+            };
+            audio.onloadeddata = () => {
+              loadedCount++;
+              const progress = (loadedCount / sounds.length) * 100;
+              loadingBar.style.width = `${progress}%`;
+              if (loadedCount === sounds.length) {
+                setTimeout(() => {
+                  loadingContainer.style.display = 'none';
+                }, 500);
+              }
+            };
           }
-        };
-        audioElements[sound.name] = audio;
 
-        const buttonContainer = document.createElement('div');
-        buttonContainer.classList.add('button-container');
+          const buttonContainer = document.createElement('div');
+          buttonContainer.classList.add('button-container');
 
-        const button = document.createElement('button');
-        button.classList.add('sound-button');
-        button.textContent = sound.name;
-        button.addEventListener('click', () => {
-          if (currentAudio && !currentAudio.paused) {
-            currentAudio.pause();
-            currentAudio.currentTime = 0;
+          const button = document.createElement('button');
+          button.classList.add('sound-button');
+          const parts = sound.name.split('/')
+          if (parts.length > 1) {
+            getOrCreateHeading(parts[0])
           }
-          audio.play();
-          currentAudio = audio;
+          button.textContent = parts.pop();
+          button.addEventListener('click', () => {
+            if (sound.type === 'folder') {
+              currentPath = sound.relativePath;
+              breadcrumb.textContent = currentPath;
+              fetchSounds(currentPath);
+            } else {
+              if (currentAudio && !currentAudio.paused) {
+                currentAudio.pause();
+                currentAudio.currentTime = 0;
+              }
+              audio.play();
+              currentAudio = audio;
+            }
+          });
+
+          buttonContainer.appendChild(button);
+          soundboard.appendChild(buttonContainer);
         });
-        buttonContainer.appendChild(button);
-        soundboard.appendChild(buttonContainer);
       });
-    });
+  }
+
+  breadcrumb.addEventListener('click', () => {
+    if (currentPath) {
+      currentPath = path.dirname(currentPath);
+      breadcrumb.textContent = currentPath;
+      fetchSounds(currentPath);
+    }
+  });
+
+  fetchSounds(currentPath);
 });
+      
